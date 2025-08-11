abc3ecfba805aadf3b8529d6b3b2b02faca0f79a
Bekhzod <ergashovb.ds@gmail.com>
2025-08-11 10:43:56 +0500

    Update google_drive.py

--- google_drive.py
+++ google_drive.py
@@ -2,18 +2,15 @@ from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
 
 class DriveHelper:
-    def __init__(self, service_json, folder_id=None):
+    def __init__(self, credentials, folder_id=None):
+        self.service = build("drive", "v3", credentials=credentials)
         self.folder_id = folder_id
-        self.service = build("drive", "v3", credentials=service_json)
 
-    def upload_file(self, local_path, filename):
-        file_metadata = {
-            "name": filename
-        }
+    def upload_file(self, file_path, filename):
+        file_metadata = {"name": filename}
         if self.folder_id:
             file_metadata["parents"] = [self.folder_id]
-
-        media = MediaFileUpload(local_path, resumable=True)
+        media = MediaFileUpload(file_path, resumable=True)
         file = self.service.files().create(
             body=file_metadata,
             media_body=media,
@@ -24,5 +21,5 @@ class DriveHelper:
     def make_file_public(self, file_id):
         self.service.permissions().create(
             fileId=file_id,
-            body={"type": "anyone", "role": "reader"},
+            body={"role": "reader", "type": "anyone"}
         ).execute()
